b49611df8176149df3299bbc83c8d91d45bc955b
SM Anas <smanas.net@gmail.com>
2025-09-08 16:55:31 +0600

    SUCCESS PAGE AND FAILED PAGE COMPLETE

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -13,98 +13,6 @@ const store_id = "comme68bce71bf282e";
 const store_passwd = "comme68bce71bf282e@ssl";
 const is_live = false;
 
-// const createOrder = async (req, res) => {
-//   try {
-//     const {
-//       userId,
-//       cartItems,
-//       addressInfo,
-//       orderStatus,
-//       paymentMethod,
-//       paymentStatus,
-//       totalAmount,
-//       orderDate,
-//       orderUpdateDate,
-//       paymentId,
-//       payerId,
-//       cartId,
-//     } = req.body;
-
-//     const create_payment_json = {
-//       intent: "sale",
-//       payer: {
-//         payment_method: "paypal",
-//       },
-//       redirect_urls: {
-//         return_url: "http://localhost:5173/shop/paypal-return",
-//         cancel_url: "http://localhost:5173/shop/paypal-cancel",
-//       },
-//       transactions: [
-//         {
-//           item_list: {
-//             items: cartItems.map((item) => ({
-//               name: item.title,
-//               sku: item.productId,
-//               price: item.price.toFixed(2),
-//               currency: "USD",
-//               quantity: item.quantity,
-//             })),
-//           },
-//           amount: {
-//             currency: "USD",
-//             total: totalAmount.toFixed(2),
-//           },
-//           description: "description",
-//         },
-//       ],
-//     };
-
-//     paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
-//       if (error) {
-//         console.log(error);
-
-//         return res.status(500).json({
-//           success: false,
-//           message: "Error while creating paypal payment",
-//         });
-//       } else {
-//         const newlyCreatedOrder = new Order({
-//           userId,
-//           cartId,
-//           cartItems,
-//           addressInfo,
-//           orderStatus,
-//           paymentMethod,
-//           paymentStatus,
-//           totalAmount,
-//           orderDate,
-//           orderUpdateDate,
-//           paymentId,
-//           payerId,
-//         });
-
-//         await newlyCreatedOrder.save();
-
-//         const approvalURL = paymentInfo.links.find(
-//           (link) => link.rel === "approval_url"
-//         ).href;
-
-//         res.status(201).json({
-//           success: true,
-//           approvalURL,
-//           orderId: newlyCreatedOrder._id,
-//         });
-//       }
-//     });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({
-//       success: false,
-//       message: "Some error occured!",
-//     });
-//   }
-// };
-
 const successPayment = async (req, res) => {
   try {
     const { id } = req.params;
@@ -142,7 +50,9 @@ const successPayment = async (req, res) => {
 
     await order.save();
 
-    res.redirect("http://localhost:5173/shop/account");
+    res.redirect(
+      `http://localhost:5173/shop/success-payment?orderId=${order._id}&status=confirmed&amount=${order.totalAmount}`
+    );
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -173,7 +83,9 @@ const failPayment = async (req, res) => {
 
     await order.save();
 
-    res.redirect("http://localhost:5173/shop/account");
+    res.redirect(
+      `http://localhost:5173/shop/failed-payment?orderId=${order._id}&status=pending&amount=${order.totalAmount}`
+    );
   } catch (e) {
     console.log(e);
     res.status(500).json({
