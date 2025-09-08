438ede8453d823524ce983c63b82669fe57aaf5a
SM Anas <smanas.net@gmail.com>
2025-09-08 17:48:47 +0600

    cors error

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://ecommerce.smanas.net",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
